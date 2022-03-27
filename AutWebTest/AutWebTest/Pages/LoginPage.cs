8ed580bb4ba384f6cbc00d107571f531065081f1
Andrew-Kiev <42326347+Andrew-Kiev@users.noreply.github.com>
2022-03-27 18:55:41 +0300

    Login Page (#1)
    * First Test!!! Hurray!!
    
    * Login page test
    
    * Run tests in pipe-line
    
    * Optimize similar negative tests, waiters added
    
    * Try to fix 'System.TypeLoadException'
    
    * Update Error Messages, delete obsolete Helpers
    
    * #2 #3 Fix error messages
    
    * Fix spelling

---
+++ AutWebTest/AutWebTest/Pages/LoginPage.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace AutWebTest.Pages
+{
+    internal class LoginPage
+    {
+        private IWebDriver driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private IWebElement LoginField
+        {
+            get
+            {
+                return driver.FindElement(By.Id("login"));
+            }
+        }
+
+        private IWebElement PasswordField => driver.FindElement(By.Id("password"));
+        private IWebElement LoginButton => driver.FindElement(By.Id("loginBtn"));
+        public string ErrorMessage => driver.FindElement(By.Id("errorMessage")).Text;
+
+        public void Login(string login, string password)
+        {
+            string loginPageURL = driver.Url;
+            LoginField.SendKeys(login);
+            PasswordField.SendKeys(password);
+            LoginButton.Click();
+            new WebDriverWait(driver, TimeSpan.FromSeconds(2))
+            .Until(_ => driver.Url != loginPageURL || ErrorMessage != string.Empty);
+        }
+    }
+}
---
+++ AutWebTest/AutWebTest/Tests/LoginPageTests.cs
@@ -0,0 +1,49 @@
+using AutWebTest.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutWebTest.Tests
+{
+    public class LoginPageTests
+    {
+        private IWebDriver driver;
+        private LoginPage loginPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new ChromeOptions { AcceptInsecureCertificates = true };  // needed to open HTTPS pages without certificate
+            options.AddArgument("log-level=3");                                     // only log errors in github pipelines
+            options.AddArgument("--silent");                                        // less info messages in github pipelines
+
+            driver = new ChromeDriver(options);
+            driver.Url = "https://localhost:5001/";
+            loginPage = new LoginPage(driver);
+        }
+
+        [TearDown]
+        public void TearDown() => driver.Dispose();
+
+        [TestCase("", "", "Login/Password cannot be empty!", TestName = "Login With Empty Creds")]
+        [TestCase("", "Somestring", "Login/Password cannot be empty!", TestName = "Login With Empty Name and wrong password")]
+        [TestCase("", "newyork1", "Login/Password cannot be empty!", TestName = "Login With Empty Name and correct password")]
+        [TestCase("Some*$#^$$*_+", "", "Login/Password cannot be empty!", TestName = "Login With Empty Password and wrong login")]
+        [TestCase("test", "", "Login/Password cannot be empty!", TestName = "Login With Empty Password and correctlogin")]
+        [TestCase("Some login", "Some string!1234", "Incorrect Login/Password!", TestName = "Login With Invalid Credentials")]
+        [TestCase("Somelogin", "newyork1", "Incorrect Login/Password!", TestName = "Login With Invalid Name")]
+        [TestCase("test", "Somestring!1234", "Incorrect Login/Password!", TestName = "Login With Invalid Password")]
+        public void InvalidOrEmptyCreds(string login, string password, string expectedError)
+        {
+            loginPage.Login(login, password);
+            Assert.AreEqual(expectedError, loginPage.ErrorMessage);
+        }
+
+        [Test]
+        public void LoginWithValidCredentials()
+        {
+            loginPage.Login("test", "newyork1");
+            Assert.AreEqual("https://localhost:5001/Calculator", driver.Url);
+        }
+    }
+}
