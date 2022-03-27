e284184a26efca43e58cd3d14b2f3c3df138d23f
Andrew-Kiev <gorodemail@gmail.com>
2022-03-27 17:54:50 +0300

    Update Error Messages, delete obsolete Helpers

--- AutWebTest/AutWebTest/Tests/LoginPageTests.cs
+++ AutWebTest/AutWebTest/Tests/LoginPageTests.cs
@@ -1,4 +1,3 @@
-using AutWebTest.Helpers;
 using AutWebTest.Pages;
 using NUnit.Core;
 using NUnit.Framework;
@@ -27,12 +26,14 @@ namespace AutWebTest.Tests
         [TearDown]
         public void TearDown() => driver.Dispose();
 
-        [TestCase("", "", "User not found!", TestName = "Login With Empty Creds")]
-        [TestCase("", "Somestring", "User not found!", TestName = "Login With Empty Name")]
-        [TestCase("Some*$#^$$*_+", "", "User not found!", TestName = "Login With Empty Password")]
-        [TestCase("Some login", "Some string!1234", "User not found!", TestName = "Login With Invalid Credentials")]
-        [TestCase("Somelogin", "newyork1", "Incorrect user name!", TestName = "Login With Invalid Name")]
-        [TestCase("test", "Somestring!1234", "Incorrect password!", TestName = "Login With Invalid Password")]
+        [TestCase("", "", "Login/Password cannot be enpty!", TestName = "Login With Empty Creds")]
+        [TestCase("", "Somestring", "Login/Password cannot be enpty!", TestName = "Login With Empty Name and wrong password")]
+        [TestCase("", "newyork1", "Login/Password cannot be enpty!", TestName = "Login With Empty Name and correct password")]
+        [TestCase("Some*$#^$$*_+", "", "Login/Password cannot be enpty!", TestName = "Login With Empty Password and wrong login")]
+        [TestCase("test", "", "Login/Password cannot be enpty!", TestName = "Login With Empty Password and correctlogin")]
+        [TestCase("Some login", "Some string!1234", "Incorrect Login/Password!", TestName = "Login With Invalid Credentials")]
+        [TestCase("Somelogin", "newyork1", "Incorrect Login/Password!", TestName = "Login With Invalid Name")]
+        [TestCase("test", "Somestring!1234", "Incorrect Login/Password!", TestName = "Login With Invalid Password")]
         public void InvalidOrEmptyCreds(string login, string password, string expectedError)
         {
             loginPage.Login(login, password);
